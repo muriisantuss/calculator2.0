1149fd9632121ac84d919a21d32a4acf67892156
muriisantuss <muriisantuss@gmail.com>
2024-02-28 15:35:31 -0300

    create fullname component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FullnameComponent } from './fullname/fullname.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullnameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fullname/fullname.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fullname',
+  templateUrl: './fullname.component.html',
+  styleUrl: './fullname.component.css'
+})
+export class FullnameComponent {
+
+}
