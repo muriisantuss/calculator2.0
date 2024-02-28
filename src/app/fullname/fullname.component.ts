a566bc155c1ed8de6ad03877ef5a2f6d65690297
muriisantuss <muriisantuss@gmail.com>
2024-02-28 15:47:23 -0300

    create a simple calculator.

--- src/app/fullname/fullname.component.ts
+++ src/app/fullname/fullname.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './fullname.component.css'
 })
 export class FullnameComponent {
+  n1: number = 0;
+  n2: number = 0;
+  sum: number = 0;
+
+  calculator(){
+    this.sum = this.n1 + this.n2;
+  }
 
 }
