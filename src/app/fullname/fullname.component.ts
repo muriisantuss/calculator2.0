921e32c3ddf1bfe2e7e9ed9963693d94010df11b
muriisantuss <muriisantuss@gmail.com>
2024-02-28 17:13:17 -0300

    added button for sum

--- src/app/fullname/fullname.component.ts
+++ src/app/fullname/fullname.component.ts
@@ -3,15 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-fullname',
   templateUrl: './fullname.component.html',
-  styleUrl: './fullname.component.css'
+  styleUrl: './fullname.component.css',
 })
 export class FullnameComponent {
   n1: number = 0;
   n2: number = 0;
   sum: number = 0;
 
-  calculator(){
-    this.sum = this.n1 + this.n2;
+  calculator(operation: number = 0) {
+    if (operation == 1) {
+      return (this.sum = this.n1 + this.n2);
+    } else{
+      return
+    }
   }
-
 }
